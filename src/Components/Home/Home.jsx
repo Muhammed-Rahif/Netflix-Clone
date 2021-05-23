ff79cc0e6fc1cf3e4203c062d6f8d17a404891c0
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-05-24 00:12:39 +0530

    PostersRow component created

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 // components
 import Navbar from "../Navbar/Navbar";
 import HeaderBanner from "../HeaderBanner/HeaderBanner";
+import PostersRow from "../PostersRow/PostersRow";
 
 function Home(props) {
   return (
     <React.Fragment>
       <Navbar />
       <HeaderBanner />
+      <PostersRow />
     </React.Fragment>
   );
 }
