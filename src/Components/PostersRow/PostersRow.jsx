00277a9f8312a1a57af4a29831abf80571b20087
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-06-03 21:41:44 +0530

    PostersRow component changed to responsive

--- src/Components/PostersRow/PostersRow.jsx
+++ src/Components/PostersRow/PostersRow.jsx
@@ -7,13 +7,8 @@ import axios from "../../configs/axios";
 import { baseThumbImgUrl } from "../../configs/urls";
 
 function PostersRow({ postersTitle, postersList }) {
-  const [onMobile, setOnMobile] = useState(false);
   // const [playButton, setPlayButton] = useState(true);
   const [ytVideoId, setYtVideoId] = useState();
-
-  useEffect(() => {
-    setOnMobile(Boolean(window.innerWidth < 767));
-  }, []);
 
   useEffect(() => {
     const observer = lozad();
@@ -48,121 +43,118 @@ function PostersRow({ postersTitle, postersList }) {
       <div className="posters-title">
         <h3>{postersTitle}</h3>
       </div>
-      {onMobile ? (
-        <React.Fragment>
-          {postersList.length > 0 &&
-            postersList.map((itm, indx) => {
-              if ((indx + 1) % 2 === 0) {
-                return (
-                  <div className="posters-wrapper" key={indx}>
-                    <div
-                      onClick={() => {
-                        handlePlayButtonClick(itm.id);
-                      }}
-                      className="poster-card"
-                    >
-                      <div className="dark-fade-top"></div>
-                      <img
-                        className="img lozad"
-                        data-src={baseThumbImgUrl + itm.poster_path}
-                        style={{ backgroundColor: "#222222e6" }}
-                        alt={itm.title}
-                      />
-                      <div className="play-button-wrapper">
-                        {/* <button
-                          onClick={() => {
-                            handlePlayButtonClick(itm.id);
-                          }}
-                        >
-                          <i className="fab fa-youtube"></i>
-                        </button> */}
-                      </div>
-                      <div className="poster-text-wrapper">
-                        <h5 className="poster-title">{prevItm.title}</h5>
-                        <p className="poster-subtitle">
-                          {prevItm.overview.substring(0, 90) + "..."}
-                        </p>
-                      </div>
-                      <div className="dark-fade-bottom"></div>
-                    </div>
-                    <div
-                      onClick={() => {
-                        handlePlayButtonClick(prevItm.id);
-                      }}
-                      className="poster-card"
-                    >
-                      <div className="dark-fade-top"></div>
-                      <img
-                        className="img lozad"
-                        data-src={baseThumbImgUrl + prevItm.poster_path}
-                        style={{ backgroundColor: "#222222e6" }}
-                        alt={itm.title}
-                      />
-                      <div className="play-button-wrapper">
-                        {/* <button
-                          onClick={() => {
-                            handlePlayButtonClick(itm.id);
-                          }}
-                        >
-                          <i className="fab fa-youtube"></i>
-                        </button> */}
-                      </div>
-                      <div className="poster-text-wrapper">
-                        <h5 className="poster-title">{itm.title}</h5>
-                        <p className="poster-subtitle">
-                          {itm.overview.substring(0, 90) + "..."}
-                        </p>
-                      </div>
-                      <div className="dark-fade-bottom"></div>
-                    </div>
-                  </div>
-                );
-              } else {
-                prevItm = itm;
-                return null;
-              }
-            })}
-        </React.Fragment>
-      ) : (
-        <div className="posters-wrapper">
-          {postersList.length > 0 &&
-            postersList.map((itm, indx) => {
+      <div className="small-device">
+        {postersList.length > 0 &&
+          postersList.map((itm, indx) => {
+            if ((indx + 1) % 2 === 0) {
               return (
-                <div
-                  onClick={() => {
-                    handlePlayButtonClick(itm.id);
-                  }}
-                  className="poster-card"
-                  key={indx}
-                >
-                  <div className="dark-fade-top"></div>
-                  <img
-                    className="img lozad"
-                    data-src={baseThumbImgUrl + itm.poster_path}
-                    style={{ background: "#222222e6" }}
-                    alt={itm.title}
-                  />
-                  <div className="play-button-wrapper">
-                    {/* <button
+                <div className="posters-wrapper" key={indx}>
+                  <div
+                    onClick={() => {
+                      handlePlayButtonClick(itm.id);
+                    }}
+                    className="poster-card"
+                  >
+                    <div className="dark-fade-top"></div>
+                    <img
+                      className="img lozad"
+                      data-src={baseThumbImgUrl + itm.poster_path}
+                      style={{ backgroundColor: "#222222e6" }}
+                      alt={itm.title}
+                    />
+                    <div className="play-button-wrapper">
+                      {/* <button
+                          onClick={() => {
+                            handlePlayButtonClick(itm.id);
+                          }}
+                        >
+                          <i className="fab fa-youtube"></i>
+                        </button> */}
+                    </div>
+                    <div className="poster-text-wrapper">
+                      <h5 className="poster-title">{prevItm.title}</h5>
+                      <p className="poster-subtitle">
+                        {prevItm.overview.substring(0, 90) + "..."}
+                      </p>
+                    </div>
+                    <div className="dark-fade-bottom"></div>
+                  </div>
+                  <div
+                    onClick={() => {
+                      handlePlayButtonClick(prevItm.id);
+                    }}
+                    className="poster-card"
+                  >
+                    <div className="dark-fade-top"></div>
+                    <img
+                      className="img lozad"
+                      data-src={baseThumbImgUrl + prevItm.poster_path}
+                      style={{ backgroundColor: "#222222e6" }}
+                      alt={itm.title}
+                    />
+                    <div className="play-button-wrapper">
+                      {/* <button
+                          onClick={() => {
+                            handlePlayButtonClick(itm.id);
+                          }}
+                        >
+                          <i className="fab fa-youtube"></i>
+                        </button> */}
+                    </div>
+                    <div className="poster-text-wrapper">
+                      <h5 className="poster-title">{itm.title}</h5>
+                      <p className="poster-subtitle">
+                        {itm.overview.substring(0, 90) + "..."}
+                      </p>
+                    </div>
+                    <div className="dark-fade-bottom"></div>
+                  </div>
+                </div>
+              );
+            } else {
+              prevItm = itm;
+              return null;
+            }
+          })}
+      </div>
+      <div className="posters-wrapper large-device">
+        {postersList.length > 0 &&
+          postersList.map((itm, indx) => {
+            return (
+              <div
+                onClick={() => {
+                  handlePlayButtonClick(itm.id);
+                }}
+                className="poster-card"
+                key={indx}
+              >
+                <div className="dark-fade-top"></div>
+                <img
+                  className="img lozad"
+                  data-src={baseThumbImgUrl + itm.poster_path}
+                  style={{ background: "#222222e6" }}
+                  alt={itm.title}
+                />
+                <div className="play-button-wrapper">
+                  {/* <button
                       onClick={() => {
                         handlePlayButtonClick(itm.id);
                       }}
                     >
                       <i className="fab fa-youtube"></i>
                     </button> */}
-                  </div>
-                  <div className="poster-text-wrapper">
-                    <h5 className="poster-title">{itm.title}</h5>
-                    <p className="poster-subtitle">
-                      {itm.overview.substring(0, 75) + "..."}
-                    </p>
-                  </div>
-                  <div className="dark-fade-bottom"></div>
                 </div>
-              );
-            })}
-        </div>
-      )}
+                <div className="poster-text-wrapper">
+                  <h5 className="poster-title">{itm.title}</h5>
+                  <p className="poster-subtitle">
+                    {itm.overview.substring(0, 75) + "..."}
+                  </p>
+                </div>
+                <div className="dark-fade-bottom"></div>
+              </div>
+            );
+          })}
+      </div>
       {ytVideoId && (
         <YouTube
           videoId={ytVideoId}
